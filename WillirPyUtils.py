debb707e4395262cdb0f7215e13b8a8a6fbb5193
Developer <developer@romos.ru>
2013-04-26 18:48:31 +0400

    Add WillirPyUtils.py wilFileDiff.py

---
+++ WillirPyUtils.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import subprocess;
+
+class CmdError(BaseException):
+    returnCode = 0;
+    cmd = "";
+    out = "";
+    err = "";
+
+    def __init__(self, returnCode, cmd, out, err):
+        self.returnCode = returnCode;
+        self.cmd = cmd;
+        self.out = out;
+        self.err = err;
+
+    def __str__(self):
+        return "Error( "+ str(self.returnCode) + ") while execution command:'" + cmd + "'\n" + \
+               "out:" + out + "\n" + "err:" + err;
+
+
+def runCommand(cmd, cwd=None, exception=False):
+    '''
+    Runs command (cmd), from (cwd) folder and 
+    @param cmd:       String for execution
+    @param cwd:       Path to dit, from which command was executed.
+    @param exception: If true, than if command error will occur, than exception will raise.
+    @return: Tuple (returnCode, out, err) where:
+        returnCode - Exit code, Result of command (0 if all ok)
+        out - String, which contains stdout of command
+        err - String, which contains stderr of command
+    @throw: WillirPyUtils.CmdError if exception == True and returnCode != 0.
+    '''
+    if cwd == None:
+        cwd = os.getcwd();
+
+    if not isinstance(cmd, str):
+        raise ValueError('cmd shall be string');
+
+    cmd = ['/bin/bash', '-c', cmd + ';'];
+
+    shell = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=cwd);
+    out, err = shell.communicate();
+    returnCode = shell.returncode;
+    if exception and returnCode != 0:
+        raise CmdError(returnCode, out, err);
+
+    return (returnCode, out, err);
+
+#class ArgsReadableDir(argparse.Action):
+#    def __call__(self, parser, namespace, values, option_string=None):
+#        prospective_dir=values
+#        if not os.path.isdir(prospective_dir):
+#            raise argparse.ArgumentTypeError("ArgsReadableDir:{0} is not a valid path".format(prospective_dir))
+#        if os.access(prospective_dir, os.R_OK):
+#            setattr(namespace, self.dest, prospective_dir)
+#        else:
+#            raise argparse.ArgumentTypeError("ArgsReadableDir:{0} is not a readable dir".format(prospective_dir))
+
+
---
+++ wilFileDiff.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from WillirPyUtils import runCommand;
+from optparse import OptionParser;
+import os;
+import re;
+
+parser = OptionParser("FIST_DIR SECOND_DIR \n" + \
+                      "Return files list which exist in first direcory but no exist in second.");
+parser.add_option("-r", "--recursive", action="store_true", dest="recursive", 
+                  help="Lookup all directory recursively?")
+parser.add_option("-d", "--delimiter", type=str, default=" ", dest="delimiter", 
+                  help="Delimiter for output files list")
+
+(options, args) = parser.parse_args();
+recursive = options.recursive;
+delimiter = options.delimiter;
+if delimiter == '\\n':
+    delimiter = '\n';
+
+firstDir = args[0];
+secondDir = args[1];
+
+if not os.path.isdir(firstDir):
+    print firstDir, "is not dir, or is absent";
+    exit(1);
+
+if not os.path.isdir(secondDir):
+    print secondDir, "is not dir, or is absent";
+    exit(1);
+
+cmd = "find ." if recursive else "ls"
+
+(_, firstFileListS,  _) = runCommand(cmd, cwd=firstDir,  exception=True);
+(_, secondFileListS, _) = runCommand(cmd, cwd=secondDir, exception=True);
+
+removeStartPoint = lambda (x): re.sub(r"^\./+", "", x);
+
+firstFileSet = set(map(removeStartPoint, firstFileListS.split()));
+secondFileSet = set(map(removeStartPoint, secondFileListS.split()));
+
+print delimiter.join(sorted(list(firstFileSet - secondFileSet)));
+
+exit(0);
+
