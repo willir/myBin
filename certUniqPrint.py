455c9165ce6371665516e23efdd0f6dbb4816bc0
Anton Rapetov <willir29@yandex.ru>
2013-12-27 17:12:03 +0400

    Add -v options (show package name) to certUniqPrint.py

--- certUniqPrint.py
+++ certUniqPrint.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python2.7
 
-import os;
 import sys;
 import argparse;
-from WillirPy2_7Utils import getArgParseApkList, argarsePathFileRoType;
+from WillirPy2_7Utils import getArgParseApkList;
 from WillirPyUtils import runCommand;
+from getPackageName import getPackageName;
 
 def getDictApkCerts(apkList):
     return __getDictApkCerts(apkList);
@@ -25,6 +25,18 @@ def __getDictApkCerts(apkList):
             certs[cert] = set([apk]);
     return certs;
 
+def __getDictApkCertsWithInfo(apkList):
+    rawCerts = __getDictApkCerts(apkList);
+    res = dict();
+    for cert in rawCerts.iterkeys():
+        apksInfo = dict();
+        for apk in rawCerts[cert]:
+            packName = getPackageName(apk);
+            apksInfo[packName] = apk;
+        res[cert] = apksInfo;
+
+    return res;
+
 
 if __name__ == "__main__":
 
@@ -33,10 +45,19 @@ if __name__ == "__main__":
                         help="Make recursive search for all apk file in dirs?");
     parser.add_argument("-o", "--out", dest='out', type=argparse.FileType('w'), default=sys.stdout, 
                         help="File which will be contains all certs.");
-    parser.add_argument("apkList", nargs="+", action=getArgParseApkList(withSign=True), help="List of apk files.");
+    parser.add_argument("-v", "--verbose", action="store_true", dest="withInfo", 
+                        help="Print also apk package name.");
+    parser.add_argument("apkList", nargs="+", action=getArgParseApkList(withSign=True), 
+                        help="List of apk files.");
     args = parser.parse_args();
 
-    certs = __getDictApkCerts(args.apkList);
+    if args.withInfo:
+        certs = __getDictApkCertsWithInfo(args.apkList);
+        for cert in certs.iterkeys():
+            certs[cert] = map(lambda (packName, apkPath): apkPath + ' : ' + packName, certs[cert].iteritems());
+            certs[cert].sort();
+    else:
+        certs = __getDictApkCerts(args.apkList);
 
     for (cert, apkList) in certs.iteritems():
         args.out.write('\n'.join(apkList) + '\n');
